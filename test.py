d7e0f2a0124076e3ac4f031dca92cde9fb2111fb
TJN25 <nicholson.tom.n@gmail.com>
2022-02-12 12:50:31 +1300

    changing test.py

--- test.py
+++ test.py
@@ -1,52 +1,14 @@
 #!/usr/bin/python
 
-import comparativesrna as srna
+import pandas as pd
+import numpy as np
+import scipy as sp
+import matplotlib as mpl
+import Bio
 
 
-help = '''
-    foobar.py v 0.1 (August 2020) is a script for {}.
-    Usage:
-         foobar.py [options] <input> <output>
-    
-    Options:
-        -h	Display this help
-        -q  Supress messages
-    Input
-        -i	<input> the input
-        -o	<output> the output
-
-    
-'''
-
-def usage():
-    print help
-
-def rungetopts():
-    try:
-        opts, args = getopt.getopt(sys.argv[1:], "i:o:qh", ["input", "output", "quiet", "help"])
-    except getopt.GetoptError as err:
-        print(err)
-        usage()
-        sys.exit(2)
-    input = ""
-    output = ""
-    for o, a in opts:
-            if o in ("-h", "--help"):
-                usage()
-                sys.exit()
-            elif o in ("-i", "--input"):
-                input = a
-            elif o in ("-o", "--output"):
-                output = a
-            else:
-                assert False, "unhandled option"
-    if output == "":
-        print "-o <output> missing. For more help use -h"
-        sys.exit(2)
-    if input == "":
-        print "-i <input> missing. For more help use -h"
-        sys.exit(2)
-    return(input, output)
 
 
-srna.helloworld()
+dat = pd.read_csv("python_files/genera_taxonomy.csv")
+
+print(dat)
